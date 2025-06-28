10d2a3d657893c4ad044419396945735aa1cfd3e
KIMJINYOUNG <k5444862@naver.com>
2025-06-28 19:57:52 +0900

    chore: initialize project with basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">HI GSAP</h1>;
+    </div>
+  );
 };
 export default App;
